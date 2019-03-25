a2f655e233e190d4cdc3a89d41f139329a767496
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-03-25 10:35:55 -0500

    Fix bugs

--- ramtools/conductivity.py
+++ ramtools/conductivity.py
@@ -1,9 +1,9 @@
 
 
-def calc_conductivity(q=1,N,V,D_cat,D_an):
+def calc_conductivity(N,V,D_cat,D_an,q=1,T=300):
     kT = T * 1.3806488e-23
     q = q * 1.60218e-19
 
-    cond = N / V * q ** 2 * (D_cat + D_an) / kT
+    cond = N / V * q ** 2 * (D_cat[0] + D_an[0]) / kT
 
     return cond
